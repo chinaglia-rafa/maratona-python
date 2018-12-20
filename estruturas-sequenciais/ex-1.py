0b34533d5912d803889c477e365dd4c9b79ccbba
Chinaglia <chinaglia.rafa@gmail.com>
2018-12-20 19:01:16 -0200

    ex-1 adicionado

---
+++ estruturas-sequenciais/ex-1.py
@@ -0,0 +1,7 @@
+'''
+    Exercício 1 da maratona de 3 meses
+    https://wiki.python.org.br/EstruturaSequencial
+    @author Rafael Araújo Chinaglia <chinaglia.rafa@gmail.com>
+'''
+
+print('Olá, mundo!')
